284b3864caca090165cff0c74ec0ccee1eca0961
Lekhika <rlekhika.regmi@gmail.com>
2025-07-21 12:19:35 +0545

    Update todoey features

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey_flutter/screens/tasks_screen.dart';
+
+import 'models/task_data.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (BuildContext context) => TaskData(),
+      child: MaterialApp(home: TasksScreen()),
+    );
+  }
+}
---
+++ lib/widgets/tasks_list.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todoey_flutter/models/task_data.dart';
+import 'package:todoey_flutter/widgets/task_tile.dart';
+
+class TasksList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskData>(
+      //using consumer to remove need of provider.of<TasksData>
+      builder: (context, taskData, child) {
+        //builder from consumer
+        return ListView.builder(
+          itemBuilder: (context, index) {
+            final task = taskData.tasks[index];
+            return TaskTile(
+              taskTitle: task.name,
+              isChecked: task.isDone,
+              checkboxCallback: (checkboxState) {
+                taskData.updateTask(task);
+              },
+              longPressCallback: () {
+                taskData.deleteTask(task);
+              },
+            );
+          },
+          itemCount: taskData.taskCount,
+        );
+      },
+    );
+  }
+}
